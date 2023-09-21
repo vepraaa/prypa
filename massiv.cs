ae2e3dfe4b2fc90c948b453914ef03c4a6c5f448
pc <prypa>
2023-09-21 12:30:26 +0500

    messege commit

---
+++ massiv.cs
@@ -0,0 +1,16 @@
+using System;
+{
+    class Program
+    {
+        static void Main();
+        {
+            Random random = new Random
+            for (int i = 0; i<100, i++)
+            {
+                int randomNumber = random.Next(1,101)
+                Console.WriteLine(randomNumber);
+                Console.ReadKey();
+            }
+        }
+    }
+}
